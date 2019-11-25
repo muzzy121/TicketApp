5151bbe2db2bb0039fc5f44919679347bfbc38bb
Mateusz <49583849+mfudala@users.noreply.github.com>
2019-11-25 08:55:29 +0100

    Template - MF

--- src/main/java/pl/programator/tickets/Ticket.java
+++ src/main/java/pl/programator/tickets/Ticket.java
@@ -1,6 +1,9 @@
 package pl.programator.tickets;
 
 public abstract class Ticket {
+    //TODO try to implement algorithm with target and number of km
+    // to be inserted by user and then calculate price
+
     private final TicketType ticketType;
 
     public Ticket(TicketType ticketType) {
--- src/main/java/pl/programator/utils/TicketMachine.java
+++ src/main/java/pl/programator/utils/TicketMachine.java
@@ -1,4 +1,22 @@
 package pl.programator.utils;
 
 public class TicketMachine {
+    void sellTicket(){}
+    void countHowManyTicketsHasBeenSold(){}
+    void calculateRevenue(){}
+
+    void showInitialMenu(){
+        System.out.println("Please choose your option:");
+        System.out.println("1. Buy ticket");
+        System.out.println("2. Show how many tickets has been sold");
+        System.out.println("3. Calculate revenue from sold tickets");
+        System.out.println("4. Exit");
+    }
+
+    void showBuyTicketMenu(){
+        System.out.println("Which ticket would you like to buy?");
+        System.out.println("1. Bus ticket");
+        System.out.println("2. Tram ticket");
+        System.out.println("3. Train ticket");
+    }
 }
